c1bd4f761d13036088e97951a84cf82a9c70d11c
NeverEvi <thhjp.dekimasen@gmail.com>
2023-10-29 13:25:16 -0400

    cleanup

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,6 @@ const baseUrl =
 	process.env.NODE_ENV === "production"
 		? "api.wtwr.strangled.net"
 		: "http://localhost:3001";
-//const baseUrl = "https://my-json-server.typicode.com/NeverEvi/se_project_react";
 
 export function request(url, options) {
 	return fetch(url, options).then(getResponse);
@@ -66,55 +65,3 @@ export const removeCardLike = (id, user, token) => {
 		body: JSON.stringify({ user }),
 	});
 };
-
-/*export const getItems = () => {
-	const itemsApi = fetch(`${baseUrl}/items`).then(getResponse);
-	return itemsApi;
-};
-export const setItems = ({ name, imageUrl, weather }, token) => {
-	const newItem = fetch(`${baseUrl}/items`, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			authorization: `Bearer ${token}`,
-		},
-		body: JSON.stringify({
-			name,
-			imageUrl,
-			weather,
-		}),
-	}).then(getResponse);
-	return newItem;
-};
-export const removeItems = (id, token) => {
-	const removed = fetch(`${baseUrl}/items/${id}`, {
-		method: "DELETE",
-		headers: {
-			"Content-Type": "application/json",
-			authorization: `Bearer ${token}`,
-		},
-	}).then(getResponse);
-	return removed;
-};
-
-export const addCardLike = (id, user, token) => {
-	return fetch(`${baseUrl}/items/${id}/likes`, {
-		method: "PUT",
-		headers: {
-			"Content-Type": "application/json",
-			authorization: `Bearer ${token}`,
-		},
-		body: JSON.stringify({ user }),
-	}).then(getResponse);
-};
-export const removeCardLike = (id, user, token) => {
-	return fetch(`${baseUrl}/items/${id}/likes`, {
-		method: "DELETE",
-		headers: {
-			"Content-Type": "application/json",
-			authorization: `Bearer ${token}`,
-		},
-		body: JSON.stringify({ user }),
-	}).then(getResponse);
-};
-*/
